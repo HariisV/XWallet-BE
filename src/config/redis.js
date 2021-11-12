3ebe1144883c63361645a4cb292b7cb69735399f
bagus tri harjanto <bagustri15@gmail.com>
2021-11-12 07:17:05 +0700

    make it better

--- src/config/redis.js
+++ src/config/redis.js
@@ -3,7 +3,7 @@ const redis = require("redis");
 const client = redis.createClient({
   host: process.env.REDIS_HOSTNAME,
   port: process.env.REDIS_PORT,
-  // password: process.env.REDIS_PASSWORD,
+  password: process.env.REDIS_PASSWORD,
 });
 
 client.on("connect", () => {
