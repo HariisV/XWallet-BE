678db0ed716c9176e22422ae9d0ab24f65a1993d
bagus tri harjanto <bagustri15@gmail.com>
2021-11-17 18:10:33 +0700

    update env

--- src/helpers/mail/index.js
+++ src/helpers/mail/index.js
@@ -22,17 +22,6 @@ const sendMail = (data) =>
 
     const accessToken = OAuth2_client.getAccessToken;
 
-    // https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4N78ToeMZfGrjVbk-kR95NcXdeZBPjuMIbllyX9vO7Te7z9HXQNo8LA0WUR_ACHHXAg2l4G67dmKBk8loxwQiA_wxRYvQ
-    // const transporter = nodemailer.createTransport({
-    //   host: 'smtp.gmail.com',
-    //   port: 465,
-    //   secure: true, // true for 465, false for other ports
-    //   auth: {
-    //     user: process.env.SMTP_EMAIL, // generated ethereal user
-    //     pass: process.env.SMTP_PASSWORD, // generated ethereal password
-    //   },
-    // });
-
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
@@ -50,14 +39,12 @@ const sendMail = (data) =>
       from: '"ZWallet 👻" <memo.in.aja@gmail.com>', // sender address
       to: to, // list of receivers
       subject: subject, // Subject line
-      // html: `<b>Click Here to activate </b><a href='http://localhost:3001/api/v1/user-activation/${to}'>Click !</>`, // html body
       html: mustache.render(fileTemplate, { ...data }),
     };
     transporter.sendMail(mailOptions, async (error, info) => {
       if (error) {
         reject(error);
       } else {
-        // console.log("Email sent:" + info.response);
         resolve(info.response);
       }
     });
